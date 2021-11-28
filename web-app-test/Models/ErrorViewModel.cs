d01c9502273243d0d7221364ef1cd4f34143d4be
Dante Rubinaccio <dante@MBP-di-Dante.home>
2021-11-28 09:53:38 +0100

    first commit

---
+++ web-app-test/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace web_app_test.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
