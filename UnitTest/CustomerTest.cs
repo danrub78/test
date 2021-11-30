c7f74489d8542479ab55d5fd91cbfe6edd60a712
Dante Rubinaccio <dante@MBP-di-Dante.home>
2021-11-30 11:49:31 +0100

    add unit test

---
+++ UnitTest/CustomerTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using web_app_test.Models;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public void TestNewCustomer()
+        {
+            Customer c = new Customer(1, "pippo");
+            Assert.AreEqual(1, c.Id);
+            Assert.AreEqual("pippo", c.Name);
+        }
+    }
+}
---
+++ web-app-test/Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace web_app_test.Models
+{
+
+    public class Customer
+    {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+
+        public Customer(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
